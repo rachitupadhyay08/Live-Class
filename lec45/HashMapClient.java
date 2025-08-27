64f8030e94c4673a982ff33b5c7f044f1484f3f2
Rachit Upadhyay <urachit08@gmail.com>
2025-08-27 12:47:58 +0530

    27 Aug

---
+++ lec45/HashMapClient.java
@@ -0,0 +1,15 @@
+package lec45;
+
+public class HashMapClient {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("RAj",77);
+        map.put("Kunal",67);
+        map.put("Ankit",97);
+        map.put("Puneet",55);
+        map.put("Pcr",39);
+        map.put("Amisha",39);
+        map.put("null",44);
+        System.out.println(map);
+    }
+}
