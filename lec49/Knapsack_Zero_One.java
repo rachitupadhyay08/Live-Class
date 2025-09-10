5c8f4f5db63b2901d712a44c0099e1b228f46611
Rachit Upadhyay <urachit08@gmail.com>
2025-09-10 10:57:42 +0530

    10 sept

---
+++ lec49/Knapsack_Zero_One.java
@@ -0,0 +1,20 @@
+package lec49;
+
+public class Knapsack_Zero_One {
+    public static void main(String[] args) {
+        int cap = 4;
+        int[] wt = {1,2,3,2,2};
+        int[] val = {8,4,0,5,3};
+    }
+
+    public static int Knapsack(int[] wt,int[] val,int cap,int i){
+        int inc=0,exc=0;
+        if(cap>=wt[i]){
+            inc=val[i]+Knapsack(wt,val,cap-wt[i],i+1);
+        }
+        exc=Knapsack(wt,val,cap,i+1);
+
+        return Math.max(inc,exc);
+    }
+    
+}
