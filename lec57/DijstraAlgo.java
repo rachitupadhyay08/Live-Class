920f74b9938c66dd9a70311f8ed01dbaa65572a3
Rachit Upadhyay <urachit08@gmail.com>
2025-11-12 12:51:57 +0530

    Dijkstra And Bellman Ford Algo

---
+++ lec57/DijstraAlgo.java
@@ -0,0 +1,71 @@
+package lec57;
+import java.util.*;
+
+public class DijstraAlgo {
+    private HashMap<Integer, HashMap<Integer, Integer>> map;
+
+	public DijstraAlgo(int v) {
+		this.map = new HashMap<>();
+		for (int i = 1; i <= v; i++) {
+			map.put(i, new HashMap<>());
+		}
+	}
+
+	public void AddEdge(int v1, int v2, int cost) {
+		map.get(v1).put(v2, cost);
+		map.get(v2).put(v1, cost);
+	}
+
+	public class DijkstraPair{
+		int vtx;
+		String acqpath;
+		int cost;
+
+		public DijkstraPair(int vtx,String acq_path,int cost){
+			this.vtx = vtx;
+			this.acqpath = acq_path;
+			this.cost=cost;
+		}
+
+		@Override
+		public String toString(){
+			return vtx + " " + acqpath + " @ " + cost;
+		}
+	}
+	public void Dijkstra(){
+		PriorityQueue<DijkstraPair> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
+		HashSet<Integer> visited = new HashSet<>();
+		pq.add(new DijkstraPair(1, "1", 0));
+		while(!pq.isEmpty()){
+			// 1. Remove 
+			DijkstraPair rp = pq.poll();
+			// 2. Ignore if Already visited
+			if(visited.contains(rp.vtx)){
+				continue;
+			}
+			// 3. Marked visited
+			visited.add(rp.vtx);
+			// 4. Self Work
+			System.out.println(rp);
+			// 5. Add nbrs
+			for(int nbrs:map.get(rp.vtx).keySet()){
+				if(!visited.contains(nbrs)){
+					int cost = map.get(rp.vtx).get(nbrs);
+					pq.add(new DijkstraPair(nbrs,rp.acqpath + nbrs, rp.cost+cost));
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		DijstraAlgo dj = new DijstraAlgo(7);
+		dj.AddEdge(1, 2, 2);
+		dj.AddEdge(2, 3, 3);
+		dj.AddEdge(1, 4, 10);
+		dj.AddEdge(4, 5, 8);
+		dj.AddEdge(3, 4, 1);
+		dj.AddEdge(5, 6, 5);
+		dj.AddEdge(5, 7, 6);
+		dj.AddEdge(6, 7, 4);
+		dj.Dijkstra();
+	}
+}
