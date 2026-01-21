0a620e4396ae14d91dac54bd7fddeb7d133cb175
Rachit Upadhyay <urachit08@gmail.com>
2026-01-21 11:41:58 +0530

    Bit Manipulation

---
+++ lec62/Kohab_The_Xorcist.java
@@ -0,0 +1,36 @@
+package lec62;
+
+import java.util.Scanner;
+
+public class Kohab_The_Xorcist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long u = sc.nextLong();
+        long v = sc.nextLong();
+        if(u > v | u%2 != v%2){
+            System.out.println(-1);
+        }
+        else if(u == v){
+            if( u== 0){
+                System.out.println(0);
+            }
+            else{
+                System.out.println(1);
+                System.out.println(u);
+            }
+        }
+
+        else{
+            long x = (v-u)/2;
+            if((u & x) == 0){
+                System.out.println(2);
+                System.out.println(u + x + " "+x);
+            }
+            else{
+                System.out.println(3);
+                System.out.println(u + " "+x+" "+x);
+            }
+        }
+    }
+
+}
